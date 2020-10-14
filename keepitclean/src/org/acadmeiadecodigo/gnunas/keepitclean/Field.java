80c80e5f60c049300bf74e3a7a96417e6b11157d
shoobypires <shoobypires@gmail.com>
2020-10-14 23:00:20 +0100

    added the field of the game

--- keepitclean/src/org/acadmeiadecodigo/gnunas/keepitclean/Field.java
+++ keepitclean/src/org/acadmeiadecodigo/gnunas/keepitclean/Field.java
@@ -1,7 +1,44 @@
 package org.acadmeiadecodigo.gnunas.keepitclean;
 
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
 public class Field {
     private int height;
     private int width;
+    public static final int PADDING=10;
+
+    public Field(int height,int width){
+        this.height=height;
+        this.width=width;
+    }
+
+    public void draw(){
+       Rectangle screen = new Rectangle(PADDING,PADDING,width,height);
+
+        screen.fill();
+
+       // int calcWidth = (screen.getWidth()/4)+PADDING;
+        //int calcHeight = (screen.getHeight()/4)+PADDING;
+        //Rectangle small = new Rectangle(170,100,960,540);
+
+        Picture fundo = new Picture(170,100,"fundo.jpg");
+        fundo.draw();
+        // small.setColor(Color.WHITE);
+       // small.fill();
+    }
+
+    public void move() {
+
+        Keyboard kb = new Keyboard(new Field(1280,720));
+        KeyboardEvent keyboardEvent = new KeyboardEvent();
+        keyboardEvent.setKey(KeyboardEvent.KEY_SPACE);
+        keyboardEvent.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        kb.addEventListener(keyboardEvent);
+    }
 
 }
--- keepitclean/src/org/acadmeiadecodigo/gnunas/keepitclean/Main.java
+++ keepitclean/src/org/acadmeiadecodigo/gnunas/keepitclean/Main.java
@@ -1,9 +1,14 @@
 package org.acadmeiadecodigo.gnunas.keepitclean;
 
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class Main {
     public static void main(String[] args) {
+
+        Field game = new Field(720,1280);
+        game.draw();
+
 
 
     }
